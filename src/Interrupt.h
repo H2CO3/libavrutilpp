aa5572dff33def627830c809a637079a70efb4c5
H2CO3 <arpad.goretity@gmail.com>
2012-04-04 06:55:09 +0200

    0th commit

---
+++ src/Interrupt.h
@@ -0,0 +1,15 @@
+/*
+ * Interrupt.h
+ * libavrutil++
+ *
+ * Created by Árpád Goretity on 04/04/2012.
+ * Licensed under a CreativeCommons Attribution-ShareAlike 3.0 Unported License
+ * http://creativecommons.org/licenses/by-sa/3.0/
+ */
+
+#ifndef __AVRUTILPP_INTERRUPT_H__
+#define __AVRUTILPP_INTERRUPT_H__
+
+#include <avrutil/avrutil.h>
+
+#endif /* __AVRUTILPP_INTERRUPT_H__ */
---
+++ src/avrutil++.h
@@ -0,0 +1,21 @@
+/*
+ * avrutil++.h
+ * libavrutil++
+ *
+ * Created by Árpád Goretity on 04/04/2012.
+ * Licensed under a CreativeCommons Attribution-ShareAlike 3.0 Unported License
+ * http://creativecommons.org/licenses/by-sa/3.0/
+ */
+
+#ifndef __AVRUTILPP_AVRUTILPP_H__
+#define __AVRUTILPP_AVRUTILPP_H__
+
+#include <avrutil++/base.h>
+#include <avrutil++/Interrupt.h>
+#include <avrutil++/LCD.h>
+#include <avrutil++/Port.h>
+#include <avrutil++/Servo.h>
+#include <avrutil++/Tone.h>
+#include <avrutil++/USART.h>
+
+#endif /* __AVRUTILPP_AVRUTILPP_H__ */
